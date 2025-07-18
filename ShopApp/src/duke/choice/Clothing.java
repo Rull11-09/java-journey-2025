3a36951714715ace652afff9aba8d65a001f792b
Abraham <morelosguerrero1109abraham@gmail.com>
2025-07-17 22:54:08 -0600

    Exercise 3‑2: Clothing items, tax & total

---
+++ ShopApp/src/duke/choice/Clothing.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author morel
+ */
+public class Clothing {
+   public String description;
+   public double price;
+   public String size = "M"; 
+}
--- ShopApp/src/duke/choice/ShopApp.java
+++ ShopApp/src/duke/choice/ShopApp.java
@@ -14,6 +14,7 @@ public class ShopApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        double tax = 0.2, total = 0.0;
         System.out.println("Welcome to Duke Choice Shop");
         
         Customer c1 = new Customer();
@@ -23,7 +24,24 @@ public class ShopApp {
       
         System.out.println("The custumer is "+ c1.name);
         
-              
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+        
+        item1.description = "Blue Jacket";
+        item1.price       = 20.9;
+        item1.size        = "M";
+
+        item2.description = "Orange T‑Shirt";
+        item2.price       = 10.5;
+        item2.size        = "S";
+        
+        System.out.println(item1.description + ", " + item1.price + ", " + item1.size);
+        System.out.println(item2.description + ", " + item2.price + ", " + item2.size);
+        
+        total =  (item1.price + item2.price *2 ) * (1 + tax);
+        
+        System.out.println("Total= " +total);
+
     }
     
 }
